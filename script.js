79126370d044f7713b3e853a617610acdff7651e
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-03 22:51:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const makeChange = (c) => {
   // your name here
-};
+	let change = {};
+	change.q = Math.floor(c/25);
+	c = c%25;
+
+	change.d = Math.floor(c/10);
+	c = c%25;
+
+	change.n = Math.floor(c/5);
+	c = c%25;
+
+	change.p = c;
+
+	return change;
+	
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
